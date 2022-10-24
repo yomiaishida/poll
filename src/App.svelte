b819166f751ccdfb952bd5ff313da2d14536f66e
Abayomi Aishida <yomicorperate@gmail.com>
2022-10-24 21:58:25 +0100

    Inline event handlers

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,11 @@
 		{name: 'mario', beltColor: 'orange', age: 45, id: 2},
 		{name: 'luigi', beltColor: 'brown', age: 35, id: 3}
 	]
+
+	const handleClick = (id) => {
+		// delete the person from people
+		people = people.filter((person) => person.id != id)
+	}
 </script>
 
 <main>
@@ -11,6 +16,7 @@
 		<div>
 			<h4>{person.name}</h4>
 			<p>{person.age} years old, {person.beltColor} belt.</p>
+			<button on:click={() => handleClick(person.id)}>delete</button>
 		</div>
 		{:else}
 			<p>There are no people to show...</p>
